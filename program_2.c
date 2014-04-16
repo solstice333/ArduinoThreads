beeb5d21d48274d0c9c71ea99d50cc8f1009e313
knavero <knavero@gmail.com>
2014-04-16 00:09:15 -0700

    Added blink() and out(). Ready to add thread specific code

--- program_2.c
+++ program_2.c
@@ -1,14 +1,44 @@
 #include <stdlib.h>
 #include <string.h>
+#include <util/delay.h>
 #include "globals.h"
 #include "os.h"
 
+/*
+ * Blinks the on-board LED once per second
+ */
+void blink();
+
+/*
+ * Sends "Hello " to the host machine which can be seen with the screen 
+ * command
+ */
+// TODO make this print stats later
+void out();
+
 int main() {
-   serial_init();
-
-   char *x = malloc(32);
-   strcpy(x, "Kevin Navero");
-
-   print_string(x);
+   os_init();
+   create_thread(blink);
+   create_thread(out);
+   os_start();
    return 0;
+}
+
+void blink() {
+   while (1) {
+      DDRB |= 1 << 5;
+      PORTB &= ~(1 << 5);
+      _delay_ms(1000);
+      DDRB |= 1 << 5;
+      PORTB |= 1 << 5;
+      _delay_ms(1000);
+   }
+}
+
+void out() {
+   serial_init();
+   while (1) {
+      _delay_ms(1000);
+      print_string("Hello ");
+   }
 }
--- test.c
+++ test.c
@@ -1,50 +1,10 @@
 #include <stdio.h>
 
-#define numToAsciiHex(type, num, container) {\
-   type __copy = (num), __quo = (num), __rem;\
-   int __i, __length;\
-   \
-   for (__length = 0; __copy; __length++)\
-      __copy /= 16;\
-   \
-   for (__i = __length - 1; __i >= 0; __i--) {\
-      __rem = (__quo) % 16;\
-   \
-      switch (__rem) {\
-         case 10:\
-            __rem = 'A';\
-            break;\
-         case 11:\
-            __rem = 'B';\
-            break;\
-         case 12:\
-            __rem = 'C';\
-            break;\
-         case 13:\
-            __rem = 'D';\
-            break;\
-         case 14:\
-            __rem = 'E';\
-            break;\
-         case 15:\
-            __rem = 'F';\
-            break;\
-         default:\
-            __rem += '0';\
-      }\
-      (container)[__i] = __rem;\
-      (__quo) /= 16;\
-   }\
-   (container)[__length] = 0;\
-}\
+void foo() {
+   printf("bleh\n");
+}
 
 int main() {
-   int arr[20], *runner = arr;
-
-   numToAsciiHex(int, 65120, arr);
-   while (*runner)
-      printf("%c ", *runner++);
-   printf("\n");
-
+   printf("address of foo: %d\n", foo);
    return 0;
 }
