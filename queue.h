c681e9c198aef5b597fd8c5bf0ac2910adb40de3
knavero <knavero@gmail.com>
2014-05-03 13:28:14 -0700

    Added comments to queue.h

--- queue.h
+++ queue.h
@@ -6,18 +6,37 @@
 #include <string.h>
 #include "globals.h"
 
+// Queue struct
 typedef struct Queue {
    void *queue;
    int head, tail;
    int queue_size, queue_size_max, element_size;
 } Queue;
 
+/*
+ * Queue constructor. |size| specifies the max size of the queue. 
+ * |element_size| specifices the element size
+ */
 Queue *Queue_create(int size, int element_size);
+
+/*
+ * Push element pointed to by |element| to back of queue |q|
+ */
 bool Queue_push(Queue *q, void *element);
+
+// Pop front element of queue |q|. User needs to free the returned element.
 void *Queue_pop(Queue *q);
+
+// Peek front element of queue |q|. User needs to free the returned element. 
 void *Queue_peek(Queue *q);
+
+// Get the number of elements inside the queue |q|
 int Queue_size(Queue *q);
+
+// Check if queue |q| is empty. true if empty, false otherwise
 bool Queue_empty(Queue *q);
+
+// Queue destructor. Destroys queue |q|
 void Queue_destroy(Queue *q);
 
 #define Queue_print(q, type) {\
